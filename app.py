ce44a1b6985dc379d7e471eab3087a15cbffa5c8
Mohan R <ragamsettymohankumar@gmail.com>
2022-09-29 22:56:16 +0530

    added comments to app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask 
+# __name__ means the name of the current python module
+
 app = Flask(__name__)
 
 
@@ -6,5 +8,9 @@ app = Flask(__name__)
 def hello():   
     return "Hello World!"
 
-if __name__ == "__main__":         
+if __name__ == "__main__":
+    """
+        When you run your Python script, Python assigns the name “__main__” to the script when executed.
+        If we import another script, the if statement will prevent other scripts from running
+    """     
     app.run()
